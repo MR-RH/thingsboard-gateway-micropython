55e93abdeb11c17a35c707a171c22799cb2cc227
MR-RH <mohammadrahbari.sch@gmail.com>
2024-08-20 08:07:10 +0330

    Feat: add TbLogger class

---
+++ tb_logger.py
@@ -0,0 +1,18 @@
+import time
+
+class TbLogger:
+    def __init__(self, name):
+        self.name = name
+
+    def log(self, level, message):
+        timestamp = "{}-{}-{} {}:{}:{}".format(*time.localtime()[:6])
+        print("{} - {} - {} - {}".format(timestamp, self.name, level, message))
+
+    def info(self, message):
+        self.log("INFO", message)
+
+    def error(self, message):
+        self.log("ERROR", message)
+
+    def debug(self, message):
+        self.log("DEBUG", message)
