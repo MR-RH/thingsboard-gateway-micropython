485f173ae0c8f41fad087477cea6de0599e2780c
MR-RH <mohammadrahbari.sch@gmail.com>
2024-08-26 14:15:16 +0330

    feat: add is_old_config_format and  _parce_attribute_info methods of backwart compatibility

--- connectors/backward_compatibility_adapter.py
+++ connectors/backward_compatibility_adapter.py
@@ -3,3 +3,20 @@ import ujson
 class BackwardCompatibilityAdapter:
     def __init__(self, config):
         self._config = ujson.loads(ujson.dumps(config))
+
+        @staticmethod
+        def _parce_attribute_info(config): 
+            attribute_name_expression = config.pop('attributeNameJsonExpression', None)
+            if attribute_name_expression:
+                config['attributeNameExpressionSource'] = 'message'
+                config['attributeNameExpression'] = attribute_name_expression
+                return
+
+            attribute_name_expression = config.pop('attributeNameTopicExpression', None)
+            if attribute_name_expression:
+                config['attributeNameExpressionSource'] = 'topic'
+                config['attributeNameExpression'] = attribute_name_expression
+
+        @staticmethod
+        def is_old_config_format(config):
+            return config.get('mapping') is not None
