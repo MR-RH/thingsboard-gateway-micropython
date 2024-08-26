6fa624b491fad9f5c88fa4058086fbe19a1aa34d
MR-RH <mohammadrahbari.sch@gmail.com>
2024-08-26 05:36:12 +0330

    feat: implement convert method in bytes_mqtt_uplink_converter

--- connectors/mqtt/bytes_mqtt_uplink_converter.py
+++ connectors/mqtt/bytes_mqtt_uplink_converter.py
@@ -1,4 +1,46 @@
+import time
+from json import dumps
+
+
 class BytesMqttUplinkConverter:
+    
+    def __init__(self, config, logger):
+        self.__config = config
+        self.logger = logger
+
+    @property
+    def config(self):
+        return self.__config
+
+    @config.setter
+    def config(self, value):
+        self.__config = value
+    
+    def convert(self, topic, data):
+        datatypes = {"attributes": "attributes",
+                     "timeseries": "telemetry"}
+        dict_result = {
+            "deviceName": self.parse_data(self.__config['deviceInfo']['deviceNameExpression'], data),
+            "deviceType": self.parse_data(self.__config['deviceInfo']['deviceProfileExpression'], data),
+            "attributes": [],
+            "telemetry": []
+        }
+
+        try:
+            for datatype in datatypes:
+                dict_result[datatypes[datatype]] = []
+                for datatype_config in self.__config.get(datatype, []):
+                    value_item = {datatype_config['key']: self.parse_data(datatype_config['value'], data)}
+                    if datatype == 'timeseries':
+                        dict_result[datatypes[datatype]].append({"ts": int(time.time()) * 1000, 'values': value_item})
+                    else:
+                        dict_result[datatypes[datatype]].append(value_item)
+        except Exception as e:
+            self.logger.error('Error in converter, for config: \n{}\n and message: \n{}\n {}'.format(dumps(self.__config),
+                            str(data), e))
+
+        self.logger.error('Converted data: {}'.format(dict_result))
+        return dict_result
     
     @staticmethod
     def parse_data(expression, data):
